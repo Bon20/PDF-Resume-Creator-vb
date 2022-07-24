2bfaa5b258ed5929d01fde906ffff454ffcb5355
Bonifacio M. De Robles <deroblesb1@gmail.com>
2022-07-24 11:48:03 +0800

    getting the info input to PDF

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,88 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnCreatePDF = New System.Windows.Forms.Button()
+        Me.lblFname = New System.Windows.Forms.Label()
+        Me.tbxFname = New System.Windows.Forms.TextBox()
+        Me.lblAge = New System.Windows.Forms.Label()
+        Me.tbxAge = New System.Windows.Forms.TextBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCreatePDF
+        '
+        Me.btnCreatePDF.Location = New System.Drawing.Point(128, 150)
+        Me.btnCreatePDF.Name = "btnCreatePDF"
+        Me.btnCreatePDF.Size = New System.Drawing.Size(94, 29)
+        Me.btnCreatePDF.TabIndex = 0
+        Me.btnCreatePDF.Text = "Create"
+        Me.btnCreatePDF.UseVisualStyleBackColor = True
+        '
+        'lblFname
+        '
+        Me.lblFname.AutoSize = True
+        Me.lblFname.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblFname.Location = New System.Drawing.Point(12, 37)
+        Me.lblFname.Name = "lblFname"
+        Me.lblFname.Size = New System.Drawing.Size(104, 28)
+        Me.lblFname.TabIndex = 1
+        Me.lblFname.Text = "Full Name:"
+        '
+        'tbxFname
+        '
+        Me.tbxFname.Location = New System.Drawing.Point(128, 41)
+        Me.tbxFname.Name = "tbxFname"
+        Me.tbxFname.Size = New System.Drawing.Size(212, 27)
+        Me.tbxFname.TabIndex = 2
+        '
+        'lblAge
+        '
+        Me.lblAge.AutoSize = True
+        Me.lblAge.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblAge.Location = New System.Drawing.Point(12, 80)
+        Me.lblAge.Name = "lblAge"
+        Me.lblAge.Size = New System.Drawing.Size(51, 28)
+        Me.lblAge.TabIndex = 1
+        Me.lblAge.Text = "Age:"
+        '
+        'tbxAge
+        '
+        Me.tbxAge.Location = New System.Drawing.Point(128, 84)
+        Me.tbxAge.Name = "tbxAge"
+        Me.tbxAge.Size = New System.Drawing.Size(212, 27)
+        Me.tbxAge.TabIndex = 2
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(246, 150)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(94, 29)
+        Me.btnExit.TabIndex = 0
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(820, 496)
+        Me.Controls.Add(Me.tbxAge)
+        Me.Controls.Add(Me.tbxFname)
+        Me.Controls.Add(Me.lblAge)
+        Me.Controls.Add(Me.lblFname)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCreatePDF)
+        Me.Name = "Form1"
+        Me.Text = "PDF Resume Creator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnCreatePDF As Button
+    Friend WithEvents lblFname As Label
+    Friend WithEvents tbxFname As TextBox
+    Friend WithEvents lblAge As Label
+    Friend WithEvents tbxAge As TextBox
+    Friend WithEvents btnExit As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,16 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports iTextSharp.text
+Imports iTextSharp.text.pdf
+Imports Newtonsoft.Json
+Public Class Form1
+    Private Sub btnCreatePDF_Click(sender As Object, e As EventArgs) Handles btnCreatePDF.Click, btnExit.Click
+        Dim PDFcreate As Document = New Document()
+        Dim Create As PdfWriter = PdfWriter.GetInstance(PDFcreate, New FileStream("DE ROBLES JR., BONIFACIO.pdf", FileMode.Create))
 
+        PDFcreate.Open()
+        PDFcreate.Add(New Paragraph(tbxFname.Text))
+        PDFcreate.Add(New Paragraph(tbxAge.Text))
+        PDFcreate.Close()
+        MessageBox.Show("Resume Created!")
+    End Sub
 End Class
